fda5294c1e4b8fd24e187cc2c5b0a992aee05228
profanis <prodromouf@gmail.com>
2020-11-18 20:10:26 +0200

    custom validators in formControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Component } from '@angular/core'
+import {
+  AbstractControl,
+  FormBuilder,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  myForm: FormGroup
+export class AppComponent {
+  firstName = this.fb.control('', Validators.required)
+  lastName = this.fb.control('', exactCharacters(20))
 
   constructor(private fb: FormBuilder) {}
+}
 
-  ngOnInit(): void {
-    this.myForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-    })
-  }
+export function exactCharacters(minCharactersValue: number): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null =>
+    control.value?.toString()?.length === minCharactersValue
+      ? null
+      : { minCharacters: control.value }
 }
