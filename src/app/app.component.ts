acee7f33f5885b9a22a472215577c6713a343285
profanis <prodromouf@gmail.com>
2020-12-01 18:02:14 +0200

    async validator in formGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core'
 import {
+  AbstractControl,
+  AsyncValidatorFn,
   FormBuilder,
   FormGroup,
   ValidationErrors,
   ValidatorFn,
   Validators,
 } from '@angular/forms'
+import { Observable, of } from 'rxjs'
+import { delay, map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -14,19 +18,35 @@ import {
 })
 export class AppComponent implements OnInit {
   myForm: FormGroup
+  private readonly existedUsernames = ['john', 'jane']
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.myForm = this.fb.group(
       {
-        password: ['', Validators.required],
+        password: this.fb.control(
+          '',
+          Validators.required,
+          this.userValidator()
+        ),
         reTypePassword: ['', Validators.required],
       },
       {
         validators: fieldsMatch('password', 'reTypePassword'),
       }
     )
+  }
+
+  userExists(username: string): Observable<boolean> {
+    return of(this.existedUsernames.includes(username)).pipe(delay(1000))
+  }
+
+  userValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> =>
+      this.userExists(control.value).pipe(
+        map((response) => (response ? { userExists: true } : null))
+      )
   }
 }
 
