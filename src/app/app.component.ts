5733bdd9466853d3257243bffe17074876efd4f9
profanis <prodromouf@gmail.com>
2021-02-06 19:04:57 +0200

    various fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,6 @@ export class AppComponent implements OnInit {
         [this.getFormArrayItem('Red'), this.getFormArrayItem()],
         {
           validators: [allValuesShouldBeDifferent('name')],
-          asyncValidators: allValuesShouldBeDifferent('name'),
         }
       ),
     })
